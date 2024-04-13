a53345b1467f16b2a90d002e663d486f1577190f
Javier Vergara <ja.vergarac96@gmail.com>
2024-04-12 21:09:38 -0400

    optimizacion de codigo

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const axios = require('axios');
-const moment = require('moment')
+const moment = require('moment');
 const uuid = require('uuid');
 const _ = require('lodash');
-const chalk = require('chalk')
+const chalk = require('chalk');
 
 const app = express();
 const PORT = 3000;
@@ -12,10 +12,13 @@ app.use(express.json());
 
 // Se crea el arreglo que almacenera como Objeto cada usuario
 const usuario = [];
+// Se declara byGenero como una variable global
+let byGenero = [];
+
 // Se asigna a la variable today la fecha y hora actual como variable global
 const today = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-app.get('/randomuser', async (req, res) => {
+app.get('/registrar', async (req, res) => {
     try {
         //leyendo una api con Axios
         const response = await axios.get('https://randomuser.me/api/');
@@ -24,29 +27,41 @@ app.get('/randomuser', async (req, res) => {
         const userData = objectData.results;
         // Se captura el Genero del usuario de la api
         const Genero = userData[0].gender;
-        // Se captura el nombre del usuario de la api en un arreglo y se junta utilizando join
-        const nombreFrag = [userData[0].name.first, userData[0].name.last];
-        const nombreCompleto = nombreFrag.join(" ");
+        // se captura el Nombre del usuario de la api
+        const Nombre = userData[0].name.first;
+        // se captura el Apellido del usuario de la api
+        const Apellido = userData[0].name.last;
         // Se genera un UUID para el usuario utilizando los 8 primeros caracteres
         const newUuid = uuid.v4().slice(0, 6);
         // Se agregan como objeto los valores capturados al arreglo por cada usuario y incluyendo la fecha actual definida como variable global
-        usuario.push({ nombreCompleto, Genero, today, newUuid })
-        // Se separa por genero en un arreglo nuevo utilizando lodash
-        const byGenero = _.partition(usuario, (n) => n.Genero == 'male');
+        usuario.push({ Nombre, Apellido, Genero, today, newUuid });
+        // Se actualiza la variable global byGenero separando el arreglo por genero
+        byGenero = _.partition(usuario, (n) => n.Genero == 'female');
 
         res.json(byGenero);
 
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching random user data' });
+    }
+});
+
+app.get('/procesar', (req, res) => {
+    try {
+
         console.log("-------------------------------------- Separador de Consultas --------------------------------------");
-        console.log("Hombres: ");
-        for (let i = 0; i < byGenero[0].length; i++){
-            const persona = chalk.yellow("Nombre: ")+byGenero[0][i].nombreCompleto+chalk.yellow(" ID: ")+byGenero[0][i].newUuid+chalk.yellow(" Timestamp: ")+byGenero[0][i].today;
-            console.log(chalk.bgWhite.blue(persona));
-        }
         console.log("Mujeres: ");
-        for (let i = 0; i < byGenero[1].length; i++){
-            const persona = chalk.yellow("Nombre: ")+byGenero[1][i].nombreCompleto+chalk.yellow(" ID: ")+byGenero[1][i].newUuid+chalk.yellow(" Timestamp: ")+byGenero[1][i].today;
-            console.log(chalk.bgWhite.blue(persona));
-        }
+        byGenero[0].map(persona => {
+            const datosPersona = chalk.yellow("Nombre: ") + persona.Nombre + chalk.yellow(" Apellido: ") + persona.Apellido + chalk.yellow(" ID: ") + persona.newUuid + chalk.yellow(" Timestamp: ") + persona.today;
+            console.log(chalk.bgWhite.blue(datosPersona));
+        });
+
+        console.log("Hombres: ");
+        byGenero[1].map(persona => {
+            const datosPersona = chalk.yellow("Nombre: ") + persona.Nombre + chalk.yellow(" Apellido: ") + persona.Apellido + chalk.yellow(" ID: ") + persona.newUuid + chalk.yellow(" Timestamp: ") + persona.today;
+            console.log(chalk.bgWhite.blue(datosPersona));
+        });
+
+        res.json(byGenero);
 
     } catch (error) {
         res.status(500).json({ error: 'Error fetching random user data' });
